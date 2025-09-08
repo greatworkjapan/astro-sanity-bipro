00573709d213d55d703d3f3d25a0a5bab65a2859
greatworkjapan <kizawa@greatwork.jp>
2025-09-08 17:52:04 +0900

    Create category.ts

---
+++ studio/schemaTypes/category.ts
@@ -0,0 +1,13 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'category',
+  title: 'Category',
+  type: 'document',
+  fields: [
+    defineField({ name: 'title', title: 'Title', type: 'string' }),
+    defineField({ name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } }),
+    defineField({ name: 'description', title: 'Description', type: 'text' }),
+    defineField({ name: 'seo', title: 'SEO', type: 'seo' })
+  ]
+})
