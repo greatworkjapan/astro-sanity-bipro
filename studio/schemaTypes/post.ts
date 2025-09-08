a5808cd630fd371761b318bdebaa40c01ca67c7c
greatworkjapan <kizawa@greatwork.jp>
2025-09-08 17:51:43 +0900

    Create post.ts

---
+++ studio/schemaTypes/post.ts
@@ -0,0 +1,32 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Blog Post',
+  type: 'document',
+  fields: [
+    defineField({ name: 'title', title: 'Title', type: 'string' }),
+    defineField({ name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } }),
+    defineField({ name: 'publishedAt', title: 'Published At', type: 'datetime' }),
+    defineField({ name: 'excerpt', title: 'Excerpt', type: 'text' }),
+    defineField({
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [{ type: 'block' }, { type: 'image' }]
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'category' }] }]
+    }),
+    defineField({
+      name: 'tags',
+      title: 'Tags',
+      type: 'array',
+      of: [{ type: 'reference', to: [{ type: 'tag' }] }]
+    }),
+    defineField({ name: 'seo', title: 'SEO', type: 'seo' })
+  ]
+})
