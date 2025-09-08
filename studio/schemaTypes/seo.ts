7d5197fe0c13aee57dcbb4225777fe4f4d2949ef
greatworkjapan <kizawa@greatwork.jp>
2025-09-08 17:52:43 +0900

    Create seo.ts

---
+++ studio/schemaTypes/seo.ts
@@ -0,0 +1,12 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'seo',
+  title: 'SEO',
+  type: 'object',
+  fields: [
+    defineField({ name: 'seoTitle', title: 'SEO Title', type: 'string' }),
+    defineField({ name: 'seoDescription', title: 'SEO Description', type: 'text' }),
+    defineField({ name: 'seoImage', title: 'SEO Image', type: 'image' })
+  ]
+})
