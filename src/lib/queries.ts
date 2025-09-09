3f5c181c189bc65a8cec4aee1e93d18c51ad5923
greatworkjapan <kizawa@greatwork.jp>
2025-09-09 12:20:40 +0900

    Create queries.ts

---
+++ src/lib/queries.ts
@@ -0,0 +1,35 @@
+export const allPostsQuery = `
+*[_type == "post"] | order(publishedAt desc){
+  _id, title, excerpt, publishedAt,
+  "slug": slug.current,
+  "categories": categories[]->{"title": title, "slug": slug.current},
+  "tags": tags[]->{"title": title, "slug": slug.current}
+}
+`
+
+export const postSlugsQuery = `*[_type=="post" && defined(slug.current)]{"slug":slug.current}`
+
+export const postBySlugQuery = `
+*[_type=="post" && slug.current==$slug][0]{
+  _id, title, excerpt, publishedAt, body,
+  "slug": slug.current,
+  "categories": categories[]->{"title": title, "slug": slug.current},
+  "tags": tags[]->{"title": title, "slug": slug.current},
+  seo
+}
+`
+
+export const categorySlugsQuery = `*[_type=="category"]{"slug":slug.current,title}`
+export const tagSlugsQuery = `*[_type=="tag"]{"slug":slug.current,title}`
+
+export const postsByCategoryQuery = `
+*[_type=="post" && $slug in categories[]->slug.current] | order(publishedAt desc){
+  title, excerpt, "slug": slug.current, publishedAt
+}
+`
+
+export const postsByTagQuery = `
+*[_type=="post" && $slug in tags[]->slug.current] | order(publishedAt desc){
+  title, excerpt, "slug": slug.current, publishedAt
+}
+`
