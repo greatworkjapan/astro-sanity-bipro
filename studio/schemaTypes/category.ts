e9d6f7dbe739713c8f6ba49260d1056dd95f1ec7
greatworkjapan <kizawa@greatwork.jp>
2025-09-11 14:40:22 +0900

    Update category.ts

--- studio/schemaTypes/category.ts
+++ studio/schemaTypes/category.ts
@@ -8,6 +8,22 @@ export default defineType({
     defineField({ name: 'title', title: 'Title', type: 'string' }),
     defineField({ name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 } }),
     defineField({ name: 'description', title: 'Description', type: 'text' }),
-    defineField({ name: 'seo', title: 'SEO', type: 'seo' })
+    defineField({ name: 'seo', title: 'SEO', type: 'seo' }),
+    defineField({
+      name: 'parent',
+      title: 'Parent Category',
+      type: 'reference',
+      to: [{ type: 'category' }],
+      options: {
+        filter: ({ document }) => {
+          return {
+            filter: '_id != $id',
+            params: {
+              id: document._id
+            }
+          }
+        }
+      }
+    }),
   ]
 })
