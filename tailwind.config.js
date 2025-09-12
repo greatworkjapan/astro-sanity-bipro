67ed79ded1362dcebdd63800b82f187a3d3886d0
greatworkjapan <kizawa@greatwork.jp>
2025-09-12 18:25:37 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,4 @@
 // tailwind.config.js
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -9,8 +7,38 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
   daisyui: {
-    themes: ["light", "dark"], // ここで有効化するテーマを指定
-  }
+    themes: [
+      {
+        dark: {
+          "primary": "#ffae9c",
+          "primary-content": "#171227",
+          "secondary": "#fff2d7",
+          "secondary-content": "#171227",
+          "accent": "#e3f1ff",
+          "accent-content": "#171227",
+          "neutral": "#2e293c",
+          "neutral-content": "#ffffff",
+          "base-100": "#171227",
+          "base-200": "#2e293c",
+          "base-content": "#ffffff",
+        },
+      },
+      {
+        light: {
+          "primary": "#ffae9c",
+          "primary-content": "#171227",
+          "secondary": "#fff2d7",
+          "secondary-content": "#171227",
+          "accent": "#e3f1ff",
+          "accent-content": "#171227",
+          "neutral": "#171227",
+          "neutral-content": "#ffffff",
+          "base-100": "#ffffff",
+          "base-200": "#d7d8e4",
+          "base-content": "#171227",
+        },
+      },
+    ],
+  },
 };
