795ee896ddf15b2c64661aa7065e57494d34a542
greatworkjapan <kizawa@greatwork.jp>
2025-09-17 18:00:52 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,9 @@
 // tailwind.config.js
-/** @type {import('tailwindcss').Config} */
 import typography from "@tailwindcss/typography";
 import daisyui from "daisyui";
 
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}"],
-  theme: {
-    extend: {},
-  },
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}"],
+  theme: { extend: {} },
   plugins: [typography, daisyui],
 };
