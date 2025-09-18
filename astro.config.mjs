62ee32e3ca263fca075a200d6b42ae5c681e4bfe
greatworkjapan <kizawa@greatwork.jp>
2025-09-18 11:19:43 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 import sanity from '@sanity/astro';
 import { sanityConfig } from './src/utils/sanity-client';
-import react from '@astrojs/react';  // ← 追加
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,11 +10,10 @@ export default defineConfig({
     domains: ['cdn.sanity.io'],
   },
   integrations: [
-    react(),                // ← React Island を有効化
+    react(),                // React Island を有効化
     sanity(sanityConfig),   // Sanity
   ],
   vite: {
-    plugins: [tailwindcss()],
     server: {
       hmr: { path: '/vite-hmr/' },
       allowedHosts: ['.netlify.app'],
